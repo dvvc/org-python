9144f1cd3bd0532920930e31845a144af90b163f
manu <manu@fastmail.co.uk>
2017-02-14 15:27:44 +0100

    add setup script for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'orgpython',
+    packages = ['orgpython'],
+    version = '0.1',
+    description = 'A library for parsing Emacs org-mode files and exporting them as HTML',
+    author = 'David Villegas',
+    author_email = 'dville00@gmail.com',
+    url = 'https://github.com/dvvc/org-python',
+    keywords = ['emacs', 'orgmode', 'parser'],
+)
